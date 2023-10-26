70bd674f38a0366621994a73afee6c94a056824c
Ganz0k <luisgonzalocervantes@gmail.com>
2023-10-25 17:34:08 -0700

    small fix in the api routes + jwt
    fixed the routes of the api for the boletos and carritos, and installed the npm jsonwebtoken

--- routes/boletoRouter.js
+++ routes/boletoRouter.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const BoletosController = require("../controllers/boletoController");
 
-router.post("/boletos", BoletosController.crearBoleto);
-router.get("/boletos/:idPelicula/:idBoleto", BoletosController.obtenerBoletoPorId);
-router.put("/boletos/:idPelicula/:idBoleto", BoletosController.actualizarBoleto);
-router.get("/boletos/:idPelicula", BoletosController.obtenerBoletosPorPelicula);
+router.post("/", BoletosController.crearBoleto);
+router.get("/:idPelicula/:idBoleto", BoletosController.obtenerBoletoPorId);
+router.put("/:idPelicula/:idBoleto", BoletosController.actualizarBoleto);
+router.get("/:idPelicula", BoletosController.obtenerBoletosPorPelicula);
 
 module.exports = router;
