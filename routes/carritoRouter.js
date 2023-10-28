599d71a098c67590f5b02c33cab1992ca9a670f7
Ganz0k <luisgonzalocervantes@gmail.com>
2023-10-28 16:25:59 -0700

    more jwt

--- routes/carritoRouter.js
+++ routes/carritoRouter.js
@@ -2,9 +2,27 @@ const express = require("express");
 const router = express.Router();
 const CarritosController = require("../controllers/carritoController");
 
-router.post("/", CarritosController.crearCarrito);
-router.get("/:id", CarritosController.obtenerCarritoPorId);
-router.put("/:id", CarritosController.actualizarCarrito);
-router.get("/:id/boletos", CarritosController.obtenerBoletosPorIdCarrito);
+router.post("/", autentificarToken, CarritosController.crearCarrito);
+router.get("/:id", autentificarToken, CarritosController.obtenerCarritoPorId);
+router.put("/:id", autentificarToken, CarritosController.actualizarCarrito);
+router.get("/:id/boletos", autentificarToken, CarritosController.obtenerBoletosPorIdCarrito);
+
+function autentificarToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) {
+        res.sendStatus(401);
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, cliente) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        req.cliente = cliente;
+        next();
+    });
+}
 
 module.exports = router;
