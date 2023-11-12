57ec06d4c20b45f6ef2e977cd752e671cc47a1ec
Ganz0k <luisgonzalocervantes@gmail.com>
2023-11-12 09:43:32 -0700

    Formulario de registro

---
+++ frontend/SignUpComponent/signUp.js
@@ -0,0 +1,22 @@
+export default class SignUpComponent extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: "open" });
+        this.#render(shadow);
+    }
+
+    async #render(shadow) {
+        await fetch("./SignUpComponent/signUpCinemapolis.html")
+            .then(response => response.text())
+            .then(html => {
+                shadow.innerHTML += html;
+            })
+            .catch(error => {
+                console.error("Error loading HTML: " + error);
+            });
+    }
+}
--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,9 @@
 import HeaderComponent from "./HeaderComponent/header.js";
 import FooterComponent from "./FooterComponent/footer.js";
 import LoginComponent from "./LoginComponent/login.js";
+import SignUpComponent from "./SignUpComponent/signUp.js";
 
 window.customElements.define("header-info", HeaderComponent);
 window.customElements.define("footer-info", FooterComponent);
 window.customElements.define("login-form", LoginComponent);
+window.customElements.define("sign-up-form", SignUpComponent);
